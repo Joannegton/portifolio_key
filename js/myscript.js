e586ec026cb72d73a88c11e135d84353160d4a0a
Joannegton <72559274+Joannegton@users.noreply.github.com>
2023-05-24 02:53:16 -0300

    update

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,11 @@
 
+function mudouTamanho(){
+    if (window.innerWidth >= 768){
+        itens.style.display = 'block'
+    } else{
+        itens.style.display = 'none'
+    }
+}
 function clickMenu(){
     if (itens.style.display == 'block'){
         itens.style.display = 'none'
@@ -6,3 +13,4 @@ function clickMenu(){
         itens.style.display = "block"
     }
 }
+
