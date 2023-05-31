01cafe7d79e707ba0f0989624231e414a3d998ed
Joannegton <72559274+Joannegton@users.noreply.github.com>
2023-05-31 00:57:48 -0300

    js

---
+++ admin/script.js
@@ -0,0 +1,11 @@
+let titulo = document.querySelector('#titulo')
+let descricao = document.querySelector('#descricao')
+let foto = document.querySelector('#foto')
+
+function postar(){
+    let tittle = document.querySelector('.titulo')
+    let description = document.querySelector('.descricao')
+
+    tittle.innerHTML = 'titulo'
+    description.innerHTML = descricao
+}
