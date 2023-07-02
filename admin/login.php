4b494bd9f7bb0e1f7544645f89efc3df34907180
joannegton <wtg131998@gmail.com>
2023-07-02 03:24:19 -0300

    update

--- admin/login.php
+++ admin/login.php
@@ -1,16 +1,14 @@
 <?php
-echo "O valor do campo email é: " . $_POST['login'];
-echo "O valor do campo senha é: " . $_POST['senha'];
 
+//requisição na API
 $url  = 'https://raw.githubusercontent.com/Joannegton/portifolio_key/main/data/profile.json';
 $ch = curl_init($url);
-
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $result = curl_exec($ch);
-$decod = json_decode(($result));
+$decod = json_decode(($result)); 
 
-$email = ($decod->tatuadora->email);
+$email = ($decod->tatuadora->email); //acessando item da API
 
 
 if($_POST['login'] == $email AND $_POST['senha'] == 'amoessagostosa'){
